a36ccda620aa2883dd7247855cfb909af7e4f6c4
olvx-project <112159585+olvx-project@users.noreply.github.com>
2022-09-20 19:01:54 +0530

    changed the root id

--- site.config.ts
+++ site.config.ts
@@ -2,14 +2,14 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '945f21e491264a8192d8ff63b28a45d8',
+  rootNotionPageId: '1fc1a317ffd14dedbda681752f104882',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Pages | Nextjs + Notion Starter',
+  name: 'Demo website',
   domain: 'pages-notion-nextjs.vercel.app',
   author: 'Raj Sharma',
 
